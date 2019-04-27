ab705d6e5f8abac04f1573ee45deace33af77ce1
Eliseo Marin Rimoldi <meliseo@vt.edu>
2019-04-27 17:39:58 -0400

    Add dihedral class

---
+++ MDI_metadynamics/dihedral.h
@@ -0,0 +1,21 @@
+using namespace std;
+
+class Dihedral
+{
+	private:
+		int atom1, atom2, atom3, atom4;
+	public:
+
+		Dihedral(int a1, int a2, int a3, int a4) {
+			a1 = a1;
+			a2 = a2;
+			a3 = a3;
+			a4 = a4;
+		}
+
+		double compute_dihedral() {
+  			double dihedral = 3.14;
+  			return dihedral;
+		}
+};
+
